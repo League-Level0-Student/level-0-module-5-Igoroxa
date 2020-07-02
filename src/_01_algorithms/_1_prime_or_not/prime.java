8d0bcae9d3f6d7f9fd4c4fb110df594348382a95
root <root@08e1e2b37386>
2020-07-02 23:57:10 +0000

    module 5 first task

---
+++ src/_01_algorithms/_1_prime_or_not/prime.java
@@ -0,0 +1,13 @@
+package _01_algorithms._1_prime_or_not;
+
+import javax.swing.JOptionPane;
+
+public class prime {
+public static void main(String[] args) {
+	String number = JOptionPane.showInputDialog("Enter in a number");
+	for (int i = 0; i < i; i++) {
+		if (number % )
+	}
+	
+}
+}
