22e10d9a644bb3b51c180c67e5d506e3ca63ed37
root <root@6277488ca807>
2020-07-09 23:55:48 +0000

    5

--- src/_01_algorithms/_1_prime_or_not/prime.java
+++ src/_01_algorithms/_1_prime_or_not/prime.java
@@ -5,9 +5,24 @@ import javax.swing.JOptionPane;
 public class prime {
 public static void main(String[] args) {
 	String number = JOptionPane.showInputDialog("Enter in a number");
-	for (int i = 0; i < i; i++) {
-		if (number % )
+	int number2 = Integer.parseInt(number);
+	for (int i = 2; i < number2; i++) {
+		if (number2%i == 0) {
+	 JOptionPane.showMessageDialog(null, "Composite");
+		System.exit(0);
+		}
+		
+
+		
+	
+	
+	
+	
+	
+	
+	
 	}
+	JOptionPane.showMessageDialog(null, "Prime");
 	
 }
 }
---
+++ src/_01_algorithms/_2_fibonacci/fibionacci.java
@@ -0,0 +1,86 @@
+package _01_algorithms._2_fibonacci;
+
+public class fibionacci {
+
+	public static void main(String[] args) {
+		
+		int number1 = 0;
+		int number2 = 1;
+		int sum = number1 + number2;
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println(sum);
+			number1 = number2;
+			number2 = sum;
+			sum = number1 + number2;
+			
+		
+		
+		}
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
