2269e6ed083f3c654c75fa133e5d273bbb24cb75
root <root@c18f1211940a>
2020-07-23 23:57:53 +0000

    infinite gunlet

---
+++ src/_02_nested_loops/_3_for_loop_gauntlet/gauntlet.java
@@ -0,0 +1,132 @@
+package _02_nested_loops._3_for_loop_gauntlet;
+
+import javax.swing.text.StyledEditorKit.ForegroundAction;
+
+public class gauntlet {
+public static void main(String[] args) {
+
+	for (int i = 0; i < 101; i++) {
+		
+		//System.out.println(i);
+		}
+	
+	for (int i = 100; 0 < i; i--) {
+		//System.out.println(i);
+	}
+	for (int i = 2; i < 101; i++) {
+		//if (i % 2 == 0) {
+			//System.out.println(i);
+		}
+	
+for (int i = 0; i < 101; i++) {
+	//if (i % 2 == 1) {
+		//System.out.println(i);
+}
+
+
+	for (int i = 0; i < 501; i++) {
+		//if (i % 2 == 0) {
+			//System.out.println(i + " is " + "even.");
+		//}
+		//if (i % 2 == 1) {
+				//System.out.println(i + " is " + "odd.");
+//	}
+		
+//for (int i = 0; i < 778; i++) {
+			//if (i % 7 == 0) {
+				//System.out.println(i);
+		}
+
+
+//for (int i = 2007; i < 2021; i++) {
+	
+	//System.out.println(i + " I was " + (i - 2007) );
+	
+	
+	
+	for (int i = 0; i < ; i++) {
+		
+	}
+}
+
+}
+	
+
+	
+}
+	
+    
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
